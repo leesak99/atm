465e85bb04213d9fb2d8436f8d7db625107ce421
toast <ls990318@gamil.com>
2025-01-08 18:47:15 +0900

    ✨ 출금 기능 추가
    사용자가 출금 금액 입력하고, 잔액에서 차감되는 기능추가, 잔액보다 큰 금액을 출금하려고 시도하면 잔액만큼만출금

--- atm.py
+++ atm.py
@@ -1,11 +1,7 @@
-# 기본 금액 : balance(변수명)
-# balan 3000원을 넣어주세요  돈을 넣어주세요
-# while문을 이용해서 입금, 출금, 영수증 보기, 종료라는 버튼을 누르기 전까지 계속해서 노출되도록 만들어주세요
-# 종료를 누르면 서비스를 종료한다는 메세지를 현재 잔액을 보여주세요
-
-#입금한 금액을 받는 변수 : deposit_amount
-#입금된 금액은 balance 변수에 추가되도록 코드를 작성해주세요
-# #영수증에 다음 순서로 값이 들어가도록 코드를 만들어주세요 -> ()"입금", 입금요청액(deposit_amount), 총액(balance)) 순으로 데이터를 넣어주세요
+#출금 요청한 금액을 받는 변수 : withdraw_amount
+#출금을 요청한 금액을 balance 변수에서 뺀 결과가 들어가도록 코드를 작성해주세요
+#영수증에 다음 순서로 값이 들어가도록 코드를 만들어주세요 -> ()"출금", withdraw_amount, balance)) 순으로 데이터를 넣어주세요
+#가지고 있는 금액보다 출금을 원하는 금액 클때 가지고 있는 금액만 출금되도록 코드를 작성해주세요
 #단, 영수증에 내역은 변경되지 않아야 하며 입금 또는 출금이 진행될때마다 이력이 기록됩니다.
 #영수증 변수는 : recipts
 
@@ -22,6 +18,12 @@ while True:
         balance = balance + deposit_amount # balance += deposit_amount
         receipts.append(("입금", deposit_amount, balance))
         print(f"입금하신 금액은 {deposit_amount}원이고, 현재 잔액은 {balance}원 입니다.")
+    if num == "2" :
+        withdraw_amount = int(input("출금할 금액을 입력해주세요 : ")) #2000
+        withdraw_amount = min(balance, withdraw_amount) 
+        balance -= withdraw_amount 
+        receipts.append(("출금", withdraw_amount, balance))
+        print(f"출금하신 금액은 {withdraw_amount}원이고, 현재 잔액은 {balance}원 입니다.")
 
 print(f"서비스를 종료합니다.현재 잔액1은{balance}원입니다.")
 
